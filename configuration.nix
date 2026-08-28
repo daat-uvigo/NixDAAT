e2097c7863711eddad8d247bfb3073812e657e52
PiterWeb <piterzdev@gmail.com>
2026-08-28 16:27:34 +0200

    Permitir login ssh user ceet

--- configuration.nix
+++ configuration.nix
@@ -155,7 +155,7 @@
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
-      AllowUsers = [ "daat" ];
+      AllowUsers = [ "daat" "ceet" ];
       MaxAuthTries = 3;
       PerSourcePenalties = "crash:3600s authfail:3600s max:86400s";
     };
